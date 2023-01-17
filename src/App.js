bcf5ee5e8f49c98327d96f38ad41b9c6ab0b9503
Justus <justus.onyancha@moringaschool.com>
2023-01-17 22:52:42 +0300

    add useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 
-
+import { useEffect, useState } from 'react';
 
 export default function Counter(){
+  const [count, setCount] = useState(0);
+  const [bool, setBool] = useState(false);
+
+  useEffect(() => {
+    console.log("count changed");
+
+    return () => console.log("cleaned up cont changed");
+    
+  }, [count]);
+
+  useEffect(() =>{
+    console.log("render");
+  });
+
+ 
+
 
   return(
     <>
-      <h1>This is a test</h1>
+      <div>
+        <button onClick={() =>{
+          setCount(count + 1);
+        }} >Increment</button>
+
+        <button onClick={() => {
+          setBool(bool);
+        }} >Re-reneder</button>
+
+        <p>Count: {count}</p>
+      </div>
     </>
   )
 }
